af54aa386015106e4b568c203579419dfc01672f
Nimufu <angelonmyknees@gmail.com>
2025-08-11 22:15:19 -0300

    Very nice dirt & Grid modified to create single tiles not multitiles

--- Assets/Scripts/MultiTileGrid.cs
+++ Assets/Scripts/MultiTileGrid.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class MultiTileGrid : MonoBehaviour
 {
-    [SerializeField] GameObject _multiTileWhite, _multiTileBlack;
+    [SerializeField] GameObject _tilePrefab;
 
-    [SerializeField] int _gridSizeX, _gridSizeY, _tileSize = 1;
+    [SerializeField] int _gridSizeX, _gridSizeY;
+    [SerializeField] float _tileSize = 0.25f;
+
+    [SerializeField] Color[] _colors;
 
     void Start()
     {
@@ -13,23 +16,56 @@ public class MultiTileGrid : MonoBehaviour
 
     void SpawnMultiTiles()
     {
-        GameObject nextTile = _multiTileWhite;
+        GameObject nextTile = _tilePrefab;
 
         for(int y = 0; y < _gridSizeY; y++)
         {
             for(int x = 0; x < _gridSizeX; x++)
             {
-                if(nextTile == _multiTileWhite)
-                {
-                    nextTile = _multiTileBlack;
-                }
-                else
-                {
-                    nextTile = _multiTileWhite;
-                }
+                // if(nextTile == _multiTileWhite)
+                // {
+                //     nextTile = _multiTileBlack;
+                // }
+                // else
+                // {
+                //     nextTile = _multiTileWhite;
+                // }
                 GameObject tile = Instantiate(nextTile, new Vector2(x * _tileSize, y * _tileSize), Quaternion.identity, transform);
                 tile.name = $"Tile {x}, {y}";
+                SetTileColor(tile.GetComponent<SpriteRenderer>(), y);
             }
+        }
+    }
+
+    void SetTileColor(SpriteRenderer tileRenderer, int row)
+    {
+        tileRenderer.color = row switch
+        {
+            < 8 => _colors[0],
+            < 20 => _colors[1],
+            < 30 => _colors[2],
+            < 42 => _colors[3],
+            < 50 => _colors[4],
+            < 64 => _colors[5],
+            _ => _colors[5],
+        };
+
+        int randomNumber = Random.Range(0, 4);
+
+        switch(randomNumber)
+        {
+            case 0:
+                tileRenderer.flipX = true;
+                break;
+            case 1:
+                tileRenderer.flipY = true;
+                break;
+            case 2:
+                tileRenderer.flipX = true;
+                tileRenderer.flipY = true;
+                break;
+            default:
+                break;
         }
     }
 }
