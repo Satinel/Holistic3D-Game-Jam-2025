735780159ff854ae7fa577a192c02f1a61fd1a29
Nimufu <angelonmyknees@gmail.com>
2025-08-14 02:20:44 -0300

    Centipedes eat Lemmings and grow longer (it mostly works as far as I can tell!)

--- Assets/Scripts/Enemy/Centipede.cs
+++ Assets/Scripts/Enemy/Centipede.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Centipede : MonoBehaviour
 {
-    [SerializeField] float _moveSpeed = 5f, _minDistance = 0.1f, _followDistance = 1f;
+    [SerializeField] float _moveSpeed = 5f, _minDistance = 0.1f;
 
     int _waypointIndex = 0;
     Rigidbody2D _rigidbody2D;
@@ -10,15 +10,19 @@ public class Centipede : MonoBehaviour
     Player _player;
     WaypointManager _waypointManager;
 
-    [SerializeField] Material _headMaterial;
+    [SerializeField] Material _headMaterial, _bodyMaterial;
     [SerializeField] Obstacle _obstaclePrefab;
-    bool _isHead;
+    [SerializeField] Centipede _centipedePrefab;
+    [SerializeField] Transform _spawnPoint;
     EnemyHealth _leader;
     Centipede _follower;
+    bool _isHead;
+    public EnemyHealth EnemyHealth { get; private set; }
 
     void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        EnemyHealth = GetComponent<EnemyHealth>();
     }
 
     void OnEnable()
@@ -60,25 +64,23 @@ public class Centipede : MonoBehaviour
             _currentTarget = null;
             _waypointIndex++;
         }
-
-        // if(!_isHead && _leader)
-        // {
-        //     if(Vector2.Distance(transform.position, _leader.transform.position) > _followDistance)
-        //     {
-        //         _rigidbody2D.linearVelocity = transform.right * (_moveSpeed + 1);
-        //     }
-        // }
     }
 
     void EnemyHealth_OnAnyEnemyDestroyed(EnemyHealth enemyHealth)
     {
-        if(enemyHealth == GetComponent<EnemyHealth>())
+        if(enemyHealth == EnemyHealth)
         {
             Instantiate(_obstaclePrefab, transform.position, Quaternion.identity);
+            _follower = null;
+            _leader = null;
         }
-        if(enemyHealth == _leader)
+        else if(enemyHealth == _leader)
         {
             SetAsHead();
+        }
+        else if(_follower && enemyHealth == _follower.EnemyHealth)
+        {
+            _follower = null;
         }
     }
 
@@ -96,14 +98,16 @@ public class Centipede : MonoBehaviour
         _currentTarget = _waypointManager.Waypoints[_waypointIndex].transform;
     }
 
-    public void Setup(Player player, WaypointManager waypointManager)
+    public void Setup(Player player, WaypointManager waypointManager, int waypointIndex)
     {
         _player = player;
         _waypointManager = waypointManager;
+        _waypointIndex = waypointIndex;
     }
 
     public void SetAsHead()
     {
+        _leader = null;
         _isHead = true;
         GetComponentInChildren<MeshRenderer>().material = _headMaterial;
     }
@@ -111,10 +115,38 @@ public class Centipede : MonoBehaviour
     public void SetFollower(Centipede follower)
     {
         _follower = follower;
+        _follower.transform.position = _spawnPoint.position;
+        _follower.GetComponentInChildren<MeshRenderer>().material = _bodyMaterial;
     }
 
     public void SetLeader(EnemyHealth leader)
     {
         _leader = leader;
+    }
+
+    public void Grow()
+    {
+        if(!_follower)
+        {
+            Centipede newSegment = Instantiate(_centipedePrefab, transform.parent);
+
+            newSegment.SetLeader(GetComponent<EnemyHealth>());
+            newSegment.Setup(_player, _waypointManager, _waypointIndex);
+
+            SetFollower(newSegment);
+        }
+        else
+        {
+            _follower.Grow();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(_isHead && collision.gameObject.GetComponent<Lemming>())
+        {
+            Destroy(collision.gameObject);
+            Grow();
+        }
     }
 }
--- Assets/Scripts/Enemy/CentipedeSpawner.cs
+++ Assets/Scripts/Enemy/CentipedeSpawner.cs
@@ -6,7 +6,6 @@ public class CentipedeSpawner : MonoBehaviour
     [SerializeField] int _totalSegments;
     [SerializeField] Vector2 _spawnPosition = new();
     [SerializeField] Centipede _centipedePrefab;
-    [SerializeField] float _segmentScale = 1;
 
     Player _player;
     WaypointManager _waypointManager;
@@ -28,19 +27,20 @@ public class CentipedeSpawner : MonoBehaviour
     {
         for(int i = 0; i < _totalSegments; i++)
         {
-            Centipede newSegment = Instantiate(_centipedePrefab, new(_spawnPosition.x - i * _segmentScale, _spawnPosition.y), Quaternion.identity, transform);
+            Centipede newSegment = Instantiate(_centipedePrefab, transform);
             newSegment.name = $"Segment {i}";
 
             if(i == 0)
             {
                 newSegment.SetAsHead();
+                newSegment.transform.position = new(_spawnPosition.x, _spawnPosition.y);
             }
             else
             {
                 newSegment.SetLeader(_previousSegment.GetComponent<EnemyHealth>());
                 _previousSegment.SetFollower(newSegment);
             }
-            newSegment.Setup(_player, _waypointManager);
+            newSegment.Setup(_player, _waypointManager, 0);
             _previousSegment = newSegment;
         }
     }
