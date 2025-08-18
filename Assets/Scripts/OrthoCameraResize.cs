a82e83542754a9491ffc6a8bfead3bb445627814
Nimufu <angelonmyknees@gmail.com>
2025-08-18 20:07:14 -0300

    Added a max size value to camera resize script to be safe and also a bool when parameters are satisfied

--- Assets/Scripts/OrthoCameraResize.cs
+++ Assets/Scripts/OrthoCameraResize.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 
 public class OrthoCameraResize : MonoBehaviour
 {
+    [SerializeField] float _maxSize = 7f;
     [SerializeField] SpriteRenderer _leftBorder, _topBorder;
+    bool _adjustmentComplete;
 
     void Update()
     {
+        if(_adjustmentComplete) { return; }
+
         if(!_leftBorder.isVisible || !_topBorder.isVisible)
         {
             Camera.main.orthographicSize += 0.01f;
+
+            if(Camera.main.orthographicSize >= _maxSize)
+            {
+                _adjustmentComplete = true;
+            }
+        }
+        else
+        {
+            _adjustmentComplete = true;
         }
     }
 }
